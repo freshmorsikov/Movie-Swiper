4428332706984d25b0d3ce96e64bddcb6edb0c4a
v.podkopaev <max.astin@yandex.ru>
2025-08-31 13:55:55 +0300

    add Add loading on Matches screen

--- composeApp/src/commonMain/kotlin/com/github/freshmorsikov/moviematcher/feature/matches/ui/MatchesScreen.kt
+++ composeApp/src/commonMain/kotlin/com/github/freshmorsikov/moviematcher/feature/matches/ui/MatchesScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -80,7 +81,15 @@ fun MatchesContent(
 ) {
     MovieScaffold {
         when (state) {
-            MatchesUdf.State.Loading -> {}
+            MatchesUdf.State.Loading -> {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .size(40.dp),
+                    color = MaterialTheme.colorScheme.secondary
+                )
+            }
+
             MatchesUdf.State.NotPaired -> {
                 NotPairedContent(onAction = onAction)
             }
