d0b1565bbb057bf50a1ba4f099a922acafb68ae2
v.podkopaev <max.astin@yandex.ru>
2025-09-08 19:58:30 +0300

    update version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -86,8 +86,8 @@ android {
         applicationId = "com.github.freshmorsikov.moviematcher"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 3
+        versionName = "0.0.3"
     }
     packaging {
         resources {
