c35099e4d63007aa023b8c216c15c35a250ea26c
v.podkopaev <max.astin@yandex.ru>
2025-10-25 17:59:34 +0300

    add MovieTheme

---
+++ composeApp/src/commonMain/kotlin/com/github/freshmorsikov/moviematcher/core/ui/theme/color/Colors.kt
@@ -0,0 +1,9 @@
+package com.github.freshmorsikov.moviematcher.core.ui.theme.color
+
+import androidx.compose.ui.graphics.Color
+
+val LavenderBlue = Color(0xFF7773FB)
+val LightGrey = Color(0xFFEFEFF1)
+val White = Color(0xFFFFFFFF)
+val Grey = Color(0xFF818181)
+val Blck = Color(0xFF191919)
---
+++ composeApp/src/commonMain/kotlin/com/github/freshmorsikov/moviematcher/core/ui/theme/color/LightColorScheme.kt
@@ -0,0 +1,11 @@
+package com.github.freshmorsikov.moviematcher.core.ui.theme.color
+
+val LightColorScheme = ColorScheme(
+    primary = LavenderBlue,
+    background = LightGrey,
+    surface = White,
+    text = TextColors(
+        main = Blck,
+        variant = Grey,
+    ),
+)
