def813c66da84dca50ea2062dad867ca4b9d0fec
v.podkopaev <max.astin@yandex.ru>
2026-01-19 23:02:32 +0200

    fix deep link handling

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -2,40 +2,22 @@ import SwiftUI
 import Firebase
 import ComposeApp
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        
-        return true
-    }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        //TODO: handle
-        return true
-    }
-        
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        //TODO: handle
-        return true
-    }
-
-}
-
 @main
 struct iOSApp: App {
     
     init() {
+        FirebaseApp.configure()
         InitKt.doInitKoinIos()
         Start.shared.startApp()
     }
     
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
     var body: some Scene {
+        
         WindowGroup {
             ContentView()
+                .onOpenURL { url in
+                    ExternalUriHandler.shared.onNewUri(uri: url.absoluteString)
+                }
         }
     }
 }
