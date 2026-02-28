122e9b069a24827ce104d432be7f876b18272d3b
v.podkopaev <max.astin@yandex.ru>
2026-02-28 14:09:20 +0200

    add NoConnection screen

---
+++ composeApp/src/commonMain/kotlin/com/github/freshmorsikov/moviematcher/feature/no_connection/NoConnectionScreen.kt
@@ -0,0 +1,82 @@
+package com.github.freshmorsikov.moviematcher.feature.no_connection
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.freshmorsikov.moviematcher.core.ui.MovieScaffold
+import com.github.freshmorsikov.moviematcher.core.ui.PrimaryMovieButton
+import com.github.freshmorsikov.moviematcher.core.ui.theme.MovieTheme
+import moviematcher.composeapp.generated.resources.Res
+import moviematcher.composeapp.generated.resources.no_connection_retry
+import moviematcher.composeapp.generated.resources.no_connection_subtitle
+import moviematcher.composeapp.generated.resources.no_connection_title
+import moviematcher.composeapp.generated.resources.popcorny_no_connection
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun NoConnectionScreen(
+    onRetryClick: () -> Unit,
+) {
+    MovieScaffold {
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                modifier = Modifier.width(width = 450.dp),
+                painter = painterResource(Res.drawable.popcorny_no_connection),
+                contentDescription = null,
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 24.dp),
+                text = stringResource(Res.string.no_connection_title),
+                style = MovieTheme.typography.title16,
+                color = MovieTheme.colors.text.main,
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
+                text = stringResource(Res.string.no_connection_subtitle),
+                style = MovieTheme.typography.body14,
+                color = MovieTheme.colors.text.variant,
+                textAlign = TextAlign.Center,
+            )
+        }
+        PrimaryMovieButton(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .padding(16.dp)
+                .padding(
+                    bottom = WindowInsets.systemBars.asPaddingValues().calculateBottomPadding()
+                ),
+            text = stringResource(Res.string.no_connection_retry),
+            onClick = onRetryClick,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun NoConnectionScreenPreview() {
+    NoConnectionScreen(onRetryClick = {})
+}
